ca0a8bdd9de4b164781151eed763144eff19e220
5pedrowx1 <73367665+5pedrowx1@users.noreply.github.com>
2025-04-14 12:01:18 +0100

    Update SecureFileManager.cs

--- RenderDiscordBot/SecureFileManager.cs
+++ RenderDiscordBot/SecureFileManager.cs
@@ -1,36 +1,35 @@
-﻿using System.Security.Cryptography;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace RenderDiscordBot
 {
     public static class SecureFileManager
     {
-        private static readonly string encryptionKey = "12345678901234567890123456789012";
-
-        public static string DecryptJson(string inputPath)
+        public static string DecryptJson(string inputPath, string encryptionKey)
         {
             byte[] encryptedData = File.ReadAllBytes(inputPath);
 
             byte[] salt = new byte[16];
             Array.Copy(encryptedData, 0, salt, 0, salt.Length);
 
-            using Aes aes = Aes.Create();
-            aes.Mode = CipherMode.CBC;
-            aes.Padding = PaddingMode.PKCS7;
+            using (Aes aes = Aes.Create())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
 
-            using var keyDerivation = new Rfc2898DeriveBytes(encryptionKey, salt, 10000);
-            aes.Key = keyDerivation.GetBytes(aes.KeySize / 8);
-            aes.IV = keyDerivation.GetBytes(aes.BlockSize / 8);
+                using var keyDerivation = new Rfc2898DeriveBytes(encryptionKey, salt, 10000);
+                aes.Key = keyDerivation.GetBytes(aes.KeySize / 8);
+                aes.IV = keyDerivation.GetBytes(aes.BlockSize / 8);
 
-            using var ms = new MemoryStream(encryptedData, salt.Length, encryptedData.Length - salt.Length);
-            using var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read);
-            using var sr = new StreamReader(cs, Encoding.UTF8);
-            return sr.ReadToEnd();
+                using var ms = new MemoryStream(encryptedData, salt.Length, encryptedData.Length - salt.Length);
+                using var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read);
+                using var sr = new StreamReader(cs, Encoding.UTF8);
+                return sr.ReadToEnd();
+            }
         }
 
-        public static string EncryptJson()
+        public static string EncryptJson(string jsonPath, string encryptionKey)
         {
-            string jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "serviceAccountKey.json");
             string plainJson = File.ReadAllText(jsonPath, Encoding.UTF8);
 
             byte[] salt = new byte[16];
@@ -39,25 +38,35 @@ namespace RenderDiscordBot
                 rng.GetBytes(salt);
             }
 
-            using Aes aes = Aes.Create();
-            aes.Mode = CipherMode.CBC;
-            aes.Padding = PaddingMode.PKCS7;
-
-            using var keyDerivation = new Rfc2898DeriveBytes(encryptionKey, salt, 10000);
-            aes.Key = keyDerivation.GetBytes(aes.KeySize / 8);
-            aes.IV = keyDerivation.GetBytes(aes.BlockSize / 8);
-            using var ms = new MemoryStream();
-            ms.Write(salt, 0, salt.Length);
-
-            using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+            using (Aes aes = Aes.Create())
             {
-                using var sw = new StreamWriter(cs, Encoding.UTF8);
-                sw.Write(plainJson);
-            }
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
 
-            string encryptedFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "serviceAccountKey.enc");
-            File.WriteAllBytes(encryptedFilePath, ms.ToArray());
-            return Convert.ToBase64String(ms.ToArray());
+                using var keyDerivation = new Rfc2898DeriveBytes(encryptionKey, salt, 10000);
+                aes.Key = keyDerivation.GetBytes(aes.KeySize / 8);
+                aes.IV = keyDerivation.GetBytes(aes.BlockSize / 8);
+
+                using var ms = new MemoryStream();
+                ms.Write(salt, 0, salt.Length);
+
+                using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                using (var sw = new StreamWriter(cs, Encoding.UTF8))
+                {
+                    sw.Write(plainJson);
+                }
+
+                byte[] encryptedBytes = ms.ToArray();
+
+                string encryptedFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "serviceAccountKey.enc");
+                File.WriteAllBytes(encryptedFilePath, encryptedBytes);
+
+                string base64Encrypted = Convert.ToBase64String(encryptedBytes);
+                string encryptedBase64Path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "serviceAccountKey.base64");
+                File.WriteAllText(encryptedBase64Path, base64Encrypted, Encoding.UTF8);
+
+                return base64Encrypted;
+            }
         }
     }
 }
